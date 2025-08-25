a0ea08d8920ca23c19007b02978ba38939cb4e9b
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-08-25 17:13:29 +0600

    complete cashout form interactive

--- js/home.js
+++ js/home.js
@@ -28,6 +28,37 @@ document.getElementById('addmoney-btn').addEventListener('click',function(){
     availMoney.innerText=totalMoney;
 })
 
+// cash out 
+document.getElementById('Cashout-btn').addEventListener('click',function(){
+    console.log('clicked');
+    let pin=1234;
+    let agentNumber =document.getElementById('agent-Number');
+    let agentNumber1= agentNumber.value;
+    if(agentNumber1.length < 11){
+        alert("please enter valid account number");
+        return;
+    }
+
+    let AmountToWithdraw=document.getElementById('amount');
+    let AmountToWithdraw1= parseInt(AmountToWithdraw.value);
+    console.log(AmountToWithdraw1);
+
+    let PinNumber=document.getElementById('Pin-Number1');
+    let pinNumber1= parseInt(PinNumber.value);
+    if(pin!== pinNumber1){
+        alert("please enter valid pin number");
+        return;
+    }
+
+     let availMoney =document.getElementById('avail-money');
+    let availMoney1=parseInt(availMoney.innerText);
+    let totalMoney=availMoney1 - AmountToWithdraw1;
+    console.log(AmountToWithdraw1);
+
+    availMoney.innerText=totalMoney;
+
+})
+
 // toggle 
 document.getElementById('cash-out-form').style.display='none';
 document.getElementById('add-money-form').style.display='none';
